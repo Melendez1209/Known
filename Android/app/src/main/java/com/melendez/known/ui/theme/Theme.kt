641b19297838aaa2be9c75b1b2d3590d2e165f19
Melendez <markmelendez2008@outlook.com>
2023-05-11 23:02:14 +0800

    Refinement of theme files to support dynamic themes

--- Android/app/src/main/java/com/melendez/known/ui/theme/Theme.kt
+++ Android/app/src/main/java/com/melendez/known/ui/theme/Theme.kt
@@ -1,10 +1,16 @@
+@file:Suppress("DEPRECATION")
+
 package com.melendez.known.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 
 private val LightColors = lightColorScheme(
@@ -74,17 +80,24 @@ private val DarkColors = darkColorScheme(
 
 @Composable
 fun KnownTheme(
-  useDarkTheme: Boolean = isSystemInDarkTheme(),
-  content: @Composable() () -> Unit
+    useDarkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable() () -> Unit,
 ) {
-  val colors = if (!useDarkTheme) {
-    LightColors
-  } else {
-    DarkColors
-  }
 
-  MaterialTheme(
-    colorScheme = colors,
-    content = content
-  )
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (useDarkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        useDarkTheme -> DarkColors
+        else -> LightColors
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        content = content,
+        typography = Typography
+    )
 }
