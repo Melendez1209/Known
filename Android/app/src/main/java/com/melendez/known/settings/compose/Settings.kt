ed2f68f7a23b4a8387a859cc34ced729ac61b01d
Melendez <MarkMelendez2008@outlook.com>
2023-05-27 14:48:47 +0800

    Android:Fixed an issue where the navigation bar would not indent in large screen mode

--- Android/app/src/main/java/com/melendez/known/settings/compose/Settings.kt
+++ Android/app/src/main/java/com/melendez/known/settings/compose/Settings.kt
@@ -3,28 +3,34 @@ package com.melendez.known.settings.compose
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Abc
 import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material.icons.rounded.ArrowDropDown
 import androidx.compose.material.icons.rounded.NavigateBefore
-import androidx.compose.material3.Card
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MediumTopAppBar
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,7 +58,10 @@ fun Settings(widthSizeClass: WindowWidthSizeClass, navTotalController: NavHostCo
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Settings_Compact(navTotalController: NavHostController) {
-    Scaffold(topBar = {
+
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+
+    Column {
         CenterAlignedTopAppBar(title = {
             Text(text = stringResource(R.string.settings))
         }, navigationIcon = {
@@ -70,7 +79,8 @@ fun Settings_Compact(navTotalController: NavHostController) {
                 )
             }
         })
-    }) { Settings_Content() }
+        Settings_Content(modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection))
+    }
 }
 
 @Preview(name = "Settings_Compact", device = "id:pixel_7_pro")
@@ -82,9 +92,10 @@ fun Settings_Compact_Preview() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Settings_Medium(navTotalController: NavHostController) {
-    Scaffold(topBar = {
 
-        val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+
+    Column {
 
         MediumTopAppBar(
             title = { Text(text = stringResource(R.string.settings)) },
@@ -106,7 +117,8 @@ fun Settings_Medium(navTotalController: NavHostController) {
             },
             scrollBehavior = scrollBehavior
         )
-    }) { Settings_Content() }
+        Settings_Content(modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection))
+    }
 }
 
 @Preview(name = "Settings_Medium", device = "spec:parent=pixel_7_pro,orientation=landscape")
@@ -118,9 +130,10 @@ fun Settings_Medium_Preview() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Settings_Expanded(navTotalController: NavHostController) {
-    Scaffold(topBar = {
 
-        val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+
+    Column {
 
         LargeTopAppBar(
             title = { Text(text = stringResource(R.string.settings)) },
@@ -142,8 +155,10 @@ fun Settings_Expanded(navTotalController: NavHostController) {
             },
             scrollBehavior = scrollBehavior
         )
-    }) { Settings_Content() }
+        Settings_Content(modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection))
+    }
 }
+
 
 @Preview(name = "Settings_Expanded", device = "spec:width=673dp,height=841dp")
 @Composable
@@ -151,30 +166,47 @@ fun Settings_Expanded_Preview() {
     Settings_Expanded(rememberNavController())
 }
 
-@Preview(device = "id:pixel_7_pro")
 @Composable
-fun Settings_Content() {
-    Surface {
-        LazyColumn {
+fun Settings_Content(modifier: Modifier) {
+
+    val never = stringResource(id = R.string.never)
+    val selected = stringResource(id = R.string.selected)
+    val unselected = stringResource(id = R.string.unselected)
+    val always = stringResource(id = R.string.always)
+
+    var labelDisplayMode by remember { mutableStateOf(never) }
+    val labelDisplayOptions = listOf(never, selected, unselected, always)
+    var expanded by remember { mutableStateOf(false) }
+
+    Surface(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = modifier) {
             item {
-                Card(
+                Row(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 12.dp)
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Row {
+                    Text(text = stringResource(id = R.string.navigation_bar_hint))
+                    Column {
+                        Button(onClick = { expanded = true }) {
+                            Text(text = labelDisplayMode)
                             Icon(
-                                imageVector = Icons.Rounded.Abc,
-                                contentDescription = stringResource(R.string.navigation_bar_hint)
+                                imageVector = Icons.Rounded.ArrowDropDown,
+                                contentDescription = stringResource(id = R.string.navigation_bar_hint)
                             )
-                            Column {
-                                Text(text = stringResource(id = R.string.navigation_bar_hint))
-                                Text(text = stringResource(id = R.string.navigation_bar_hint))
+                        }
+                        DropdownMenu(
+                            expanded = expanded,
+                            onDismissRequest = { expanded = false }
+                        ) {
+                            labelDisplayOptions.forEach { option ->
+                                DropdownMenuItem(text = { Text(text = option) },
+                                    onClick = {
+                                        labelDisplayMode = option
+                                        expanded = false
+                                    })
                             }
                         }
                     }
@@ -182,4 +214,12 @@ fun Settings_Content() {
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(device = "id:pixel_7_pro")
+@Composable
+fun Settings_Content_Preview() {
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    Settings_Content(modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection))
 }
