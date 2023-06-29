b27beca56b61ec5c5dc8b11690af69c0352369a1
Melendez <MarkMelendez2008@outlook.com>
2023-06-29 11:33:26 +0800

    Android: In the Settiing page, use the official ListItem instead of the custom SettingRow

--- Android/app/src/main/java/com/melendez/known/settings/compose/Settings.kt
+++ Android/app/src/main/java/com/melendez/known/settings/compose/Settings.kt
@@ -1,9 +1,9 @@
 package com.melendez.known.settings.compose
 
 import android.os.Build
+import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,15 +11,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowDropDown
 import androidx.compose.material.icons.rounded.Help
 import androidx.compose.material.icons.rounded.NavigateBefore
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Surface
@@ -29,6 +32,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -47,7 +51,7 @@ import com.melendez.known.R
 fun Settings(widthSizeClass: WindowWidthSizeClass, navTotalController: NavHostController) {
     when (widthSizeClass) {
         WindowWidthSizeClass.Compact -> {
-            Settings_Compact(navTotalController = navTotalController)
+            Settings_CompactExpanded(navTotalController = navTotalController)
         }
 
         WindowWidthSizeClass.Medium -> {
@@ -55,53 +59,18 @@ fun Settings(widthSizeClass: WindowWidthSizeClass, navTotalController: NavHostCo
         }
 
         WindowWidthSizeClass.Expanded -> {
-            Settings_Compact(navTotalController = navTotalController)
+            Settings_CompactExpanded(navTotalController = navTotalController)
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Settings_Medium(navTotalController: NavHostController) {
+fun Settings_CompactExpanded(navTotalController: NavHostController) {
 
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
 
     Column {
-
-        MediumTopAppBar(
-            title = { Text(text = stringResource(R.string.settings)) },
-            navigationIcon = {
-                IconButton(onClick = { navTotalController.popBackStack() }) {
-                    Icon(
-                        imageVector = Icons.Rounded.NavigateBefore,
-                        contentDescription = stringResource(R.string.back)
-                    )
-                }
-            },
-            actions = {
-                IconButton(onClick = {
-                    //TODO:Jump to help
-                }) {
-                    Icon(
-                        imageVector = Icons.Rounded.Help,
-                        contentDescription = stringResource(R.string.sign_to)
-                    )
-                }
-            },
-            scrollBehavior = scrollBehavior
-        )
-        Settings_Content(modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection))
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun Settings_Compact(navTotalController: NavHostController) {
-
-    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
-
-    Column {
-
         LargeTopAppBar(
             title = { Text(text = stringResource(R.string.settings)) },
             navigationIcon = {
@@ -113,9 +82,11 @@ fun Settings_Compact(navTotalController: NavHostController) {
                 }
             },
             actions = {
-                IconButton(onClick = {
-                    //TODO:Jump to help
-                }) {
+                IconButton(
+                    onClick = {
+                        //TODO:Jump to help
+                    }
+                ) {
                     Icon(
                         imageVector = Icons.Rounded.Help,
                         contentDescription = stringResource(R.string.sign_to)
@@ -128,23 +99,53 @@ fun Settings_Compact(navTotalController: NavHostController) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Settings_Medium(navTotalController: NavHostController) {
+
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+
+    Column {
+        MediumTopAppBar(
+            title = { Text(text = stringResource(R.string.settings)) },
+            navigationIcon = {
+                IconButton(onClick = { navTotalController.popBackStack() }) {
+                    Icon(
+                        imageVector = Icons.Rounded.NavigateBefore,
+                        contentDescription = stringResource(R.string.back)
+                    )
+                }
+            },
+            actions = {
+                IconButton(
+                    onClick = {
+                        //TODO:Jump to help
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Help,
+                        contentDescription = stringResource(R.string.sign_to)
+                    )
+                }
+            },
+            scrollBehavior = scrollBehavior
+        )
+        Settings_Content(modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection))
+    }
+}
+
+@Suppress("UNUSED_EXPRESSION")
 @Composable
 fun Settings_Content(modifier: Modifier) {
 
+    // Variables related to settings
     var labelMode by rememberSaveable { mutableStateOf(false) }
     var colorMode by rememberSaveable { mutableStateOf(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) }
+    var grade by rememberSaveable { mutableIntStateOf(10) }
 
+    // Variables related to components
     var visibleAppearance by remember { mutableStateOf(true) }
     var visibleAnalysis by remember { mutableStateOf(true) }
-    var selected by remember { mutableStateOf(false) }
-    val gradeList = listOf<String>(
-        stringResource(id = R.string.g7),
-        stringResource(id = R.string.g8),
-        stringResource(id = R.string.g9),
-        stringResource(id = R.string.g10),
-        stringResource(id = R.string.g11),
-        stringResource(id = R.string.g12)
-    )
 
     Surface(modifier = Modifier.fillMaxSize()) {
         LazyColumn(modifier = modifier) {
@@ -154,20 +155,18 @@ fun Settings_Content(modifier: Modifier) {
                         modifier = Modifier.align(Alignment.CenterHorizontally),
                         onClick = {
                             //TODO:Login
-                        }) {
+                        }
+                    ) {
                         Text(text = stringResource(R.string.sign_to))
                     }
                 }
             }
             item {
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 12.dp, vertical = 6.dp)
-                ) {
-                    Row(modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { visibleAppearance = !visibleAppearance }) {
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable { visibleAppearance = !visibleAppearance }) {
                         Text(
                             modifier = Modifier.padding(start = 12.dp, top = 6.dp, bottom = 6.dp),
                             text = stringResource(R.string.appearance),
@@ -176,35 +175,48 @@ fun Settings_Content(modifier: Modifier) {
                     }
                     AnimatedVisibility(visible = visibleAppearance) {
                         Column {
-                            SettingRow(
-                                title = stringResource(id = R.string.hint_title),
-                                sumOn = stringResource(id = R.string.hint_on),
-                                sumOff = stringResource(id = R.string.hint_off),
-                                value = labelMode,
-                                onCheckedChange = { labelMode = it }
+                            ListItem(
+                                headlineContent = { Text(text = stringResource(id = R.string.hint_title)) },
+                                supportingContent = {
+                                    Text(
+                                        text = if (labelMode) stringResource(id = R.string.hint_on)
+                                        else stringResource(id = R.string.hint_off)
+                                    )
+                                },
+                                trailingContent = {
+                                    Switch(
+                                        checked = labelMode,
+                                        onCheckedChange = { labelMode = it }
+                                    )
+                                }
                             )
                             Divider()
-                            SettingRow(
-                                title = stringResource(R.string.dynamic_color),
-                                sumOn = stringResource(R.string.apply_wallpaper),
-                                sumUnable = stringResource(R.string.low_version),
-                                value = colorMode,
-                                enable = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S,
-                                onCheckedChange = { colorMode = it }
+                            ListItem(
+                                headlineContent = { Text(text = stringResource(R.string.dynamic_color)) },
+                                supportingContent = { Text(text = stringResource(R.string.apply_wallpaper)) },
+                                overlineContent = {
+                                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) Text(
+                                        text = stringResource(R.string.low_version)
+                                    ) else null
+                                },
+                                trailingContent = {
+                                    Switch(
+                                        enabled = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S,
+                                        checked = colorMode,
+                                        onCheckedChange = { colorMode = it })
+                                }
                             )
                         }
                     }
                 }
             }
             item {
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 12.dp, vertical = 6.dp)
-                ) {
-                    Row(modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { visibleAnalysis = !visibleAnalysis }) {
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable { visibleAnalysis = !visibleAnalysis }
+                    ) {
                         Text(
                             modifier = Modifier.padding(start = 12.dp, top = 6.dp, bottom = 6.dp),
                             text = stringResource(R.string.analysis),
@@ -213,61 +225,48 @@ fun Settings_Content(modifier: Modifier) {
                     }
                     AnimatedVisibility(visible = visibleAnalysis) {
                         Column {
-                            Text(
-                                modifier = Modifier.padding(start = 12.dp, top = 6.dp),
-                                text = stringResource(id = R.string.grade),
-                                style = MaterialTheme.typography.titleLarge
-                            )
-                            Row(
-                                modifier = Modifier
-                                    .align(Alignment.CenterHorizontally)
-                                    .fillMaxWidth(),
-                                horizontalArrangement = Arrangement.SpaceBetween
-                            ) {
 
-                            }
+                            var expanded by remember { mutableStateOf(false) }
+                            val gradeList = mutableListOf<String>(
+                                stringResource(id = R.string.g7),
+                                stringResource(id = R.string.g8),
+                                stringResource(id = R.string.g9),
+                                stringResource(id = R.string.g10),
+                                stringResource(id = R.string.g11),
+                                stringResource(id = R.string.g12)
+                            )
+
+                            ListItem(
+                                headlineContent = { Text(text = stringResource(id = R.string.grade)) },
+                                trailingContent = {
+                                    Button(onClick = { expanded = true }) {
+                                        Text(text = gradeList[grade - 7])
+                                        Icon(
+                                            imageVector = Icons.Rounded.ArrowDropDown,
+                                            contentDescription = stringResource(R.string.select_grade)
+                                        )
+                                    }
+                                    DropdownMenu(
+                                        expanded = expanded,
+                                        onDismissRequest = { expanded = false }) {
+                                        gradeList.forEach { it ->
+                                            DropdownMenuItem(
+                                                text = { Text(text = it) },
+                                                onClick = {
+                                                    grade = gradeList.indexOf(it) + 7
+                                                    expanded = false
+                                                    Log.d("TAG", "Settings_Content: grade:$grade")
+                                                }
+                                            )
+                                        }
+                                    }
+                                }
+                            )
                         }
                     }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun SettingRow(
-    title: String,
-    sumOn: String,
-    sumOff: String = sumOn,
-    sumUnable: String = "",
-    value: Boolean,
-    enable: Boolean = true,
-    onCheckedChange: ((Boolean) -> Unit)?,
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(6.dp),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Column(
-            modifier = Modifier.padding(6.dp),
-            verticalArrangement = Arrangement.spacedBy(3.dp)
-        ) {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.titleLarge
-            )
-            Text(
-                text = if (!enable) sumUnable else if (value) sumOn else sumOff,
-                style = MaterialTheme.typography.titleSmall
-            )
-        }
-        Switch(
-            enabled = enable,
-            checked = value,
-            onCheckedChange = { onCheckedChange?.invoke(it) })
     }
 }
 
@@ -281,14 +280,15 @@ fun Settings_Content_Preview() {
     )
 }
 
-@Preview(name = "Settings_Compact", device = "spec:width=673dp,height=841dp")
-@Composable
-fun Settings_Expanded_Preview() {
-    Settings_Compact(rememberNavController())
-}
-
 @Preview(name = "Settings_Medium", device = "spec:parent=pixel_7_pro,orientation=landscape")
 @Composable
 fun Settings_Medium_Preview() {
     Settings_Medium(rememberNavController())
 }
+
+@Preview(name = "Settings_CompactExpanded", device = "spec:width=673dp,height=841dp")
+@Composable
+fun Settings_Expanded_Preview() {
+    Settings_CompactExpanded(rememberNavController())
+}
+
