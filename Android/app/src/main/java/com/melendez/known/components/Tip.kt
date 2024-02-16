ae38b937158e5fb16a0cef605db23bb32a3b9363
Melendez <MarkMelendez2008@outlook.com>
2024-02-16 09:56:32 +0800

    Android: Add Tolltips to the detail page and hide the bottom bar when appropriate

---
+++ Android/app/src/main/java/com/melendez/known/components/Tip.kt
@@ -0,0 +1,24 @@
+package com.melendez.known.components
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.PlainTooltip
+import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Tip(text: String, content: @Composable () -> Unit) {
+    TooltipBox(
+        positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
+        tooltip = {
+            PlainTooltip {
+                Text(text = text)
+            }
+        },
+        state = rememberTooltipState(),
+        content = content
+    )
+}
--- Android/app/src/main/java/com/melendez/known/detail/Detail.kt
+++ Android/app/src/main/java/com/melendez/known/detail/Detail.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -17,8 +18,10 @@ import androidx.compose.material.icons.rounded.FavoriteBorder
 import androidx.compose.material.icons.rounded.Print
 import androidx.compose.material.icons.rounded.Share
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -33,12 +36,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.melendez.known.R
 import com.melendez.known.Screens
+import com.melendez.known.components.Tip
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -51,8 +56,10 @@ fun Detail(navTotalController: NavHostController) {
     )
 
     var isFavorite by remember { mutableStateOf(false) }
+    val scrollBehavior = BottomAppBarDefaults.exitAlwaysScrollBehavior()
 
     Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             TopAppBar(
                 title = { Text(text = stringResource(id = R.string.exam)) },
@@ -68,48 +75,62 @@ fun Detail(navTotalController: NavHostController) {
         bottomBar = {
             BottomAppBar(
                 actions = {
-                    IconButton(onClick = {
-                        val shareIntent = Intent(Intent.ACTION_SEND).apply {
-                            type = "text/plain"
-                            putExtra(
-                                Intent.EXTRA_TEXT,
-                                "Known"
-                            )//TODO: replace Known with detail info
+                    Tip(text = stringResource(R.string.share)) {
+                        IconButton(onClick = {
+                            val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                                type = "text/plain"
+                                putExtra(
+                                    Intent.EXTRA_TEXT,
+                                    "Known"
+                                )//TODO: replace Known with detail info
+                            }
+                            launcher.launch(shareIntent)
+                        }) {
+                            Icon(
+                                imageVector = Icons.Rounded.Share,
+                                contentDescription = stringResource(R.string.share)
+                            )
                         }
-                        launcher.launch(shareIntent)
-                    }) {
-                        Icon(
-                            imageVector = Icons.Rounded.Share,
-                            contentDescription = stringResource(R.string.share)
-                        )
                     }
-                    IconButton(onClick = { navTotalController.navigate(Screens.DRP.router) }) {
-                        Icon(
-                            imageVector = Icons.Rounded.Edit,
-                            contentDescription = stringResource(R.string.edit)
-                        )
+                    Tip(text = stringResource(R.string.edit)) {
+                        IconButton(onClick = { navTotalController.navigate(Screens.DRP.router) }) {
+                            Icon(
+                                imageVector = Icons.Rounded.Edit,
+                                contentDescription = stringResource(R.string.edit)
+                            )
+                        }
                     }
-                    IconButton(onClick = { isFavorite = !isFavorite }) {
-                        Icon(
-                            imageVector = if (!isFavorite) Icons.Rounded.FavoriteBorder else Icons.Rounded.Favorite,
-                            contentDescription = stringResource(R.string.favourite)
-                        )
+                    Tip(text = stringResource(R.string.favourite)) {
+                        IconButton(onClick = { isFavorite = !isFavorite }) {
+                            Icon(
+                                imageVector = if (!isFavorite) Icons.Rounded.FavoriteBorder else Icons.Rounded.Favorite,
+                                contentDescription = stringResource(R.string.favourite)
+                            )
+                        }
                     }
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Rounded.Delete,
-                            contentDescription = stringResource(R.string.delete)
-                        )
+                    Tip(text = stringResource(R.string.delete)) {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.Rounded.Delete,
+                                contentDescription = stringResource(R.string.delete)
+                            )
+                        }
                     }
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Rounded.Print,
-                            contentDescription = stringResource(R.string.print)
-                        )
+                    Tip(text = stringResource(R.string.print)) {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.Rounded.Print,
+                                contentDescription = stringResource(R.string.print)
+                            )
+                        }
                     }
                 },
+                scrollBehavior = scrollBehavior,
                 floatingActionButton = {
-                    FloatingActionButton(onClick = { navTotalController.navigate(Screens.Prophets.router) }) {
+                    FloatingActionButton(
+                        onClick = { navTotalController.navigate(Screens.Prophets.router) },
+                        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+                    ) {
                         Icon(
                             imageVector = Icons.AutoMirrored.Rounded.Chat,
                             contentDescription = stringResource(R.string.ai_analyse)
@@ -133,23 +154,30 @@ fun Detail(navTotalController: NavHostController) {
         )
         var course by remember { mutableIntStateOf(0) }
 
-        ScrollableTabRow(
-            selectedTabIndex = course,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = it.calculateTopPadding(), bottom = it.calculateBottomPadding())
-        ) {
-            courseList.forEachIndexed { index, title ->
-                Tab(
-                    selected = course == index,
-                    onClick = { course = index },
-                    text = { Text(text = title) }
+        Column {
+            ScrollableTabRow(
+                selectedTabIndex = course,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = it.calculateTopPadding())
+            ) {
+                courseList.forEachIndexed { index, title ->
+                    Tab(
+                        selected = course == index,
+                        onClick = { course = index },
+                        text = { Text(text = title) }
                     )
+                }
             }
-
-        }
-        LazyColumn(modifier = Modifier.padding(bottom = it.calculateBottomPadding())) {
-
+            LazyColumn(
+                modifier = Modifier
+                    .padding(bottom = it.calculateBottomPadding())
+                    .fillMaxWidth()
+            ) {
+                items(50) {
+                    Text(text = "$it")
+                }
+            }
         }
     }
 }
